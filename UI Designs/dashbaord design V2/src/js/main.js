ee689f813d6a32a70c6b2a77f867584995cff991
hazem.hemilym <hazem.hemily@gmail.com>
2018-04-20 21:06:53 +0200

    finishing login frame ,admin panels , responsiveness issues and side bar

--- UI Designs/dashbaord design V2/src/js/main.js
+++ UI Designs/dashbaord design V2/src/js/main.js
@@ -1,15 +1,16 @@
+slideInOut();
 var data = {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [{
         label: "My First dataset",
-        fill:false ,
+        fill: false,
         backgroundColor: 'rgb(255, 99, 132)',
         borderColor: 'rgb(255, 99, 132)',
         data: [0, 10, 5, 2, 20, 30, 45],
     }]
-} ; 
-const lineChart =document.getElementById("line-chart") ; 
-var CHART1 = new Chart(lineChart , {
+};
+const lineChart = document.getElementById("line-chart");
+var CHART1 = new Chart(lineChart, {
     type: 'line',
 
     // The data for our dataset
@@ -18,8 +19,8 @@ var CHART1 = new Chart(lineChart , {
     // Configuration options go here
     options: {}
 });
-const pieChart =document.getElementById("pie-chart") ; 
-var CHART2 = new Chart(pieChart , {
+const pieChart = document.getElementById("pie-chart");
+var CHART2 = new Chart(pieChart, {
     type: 'pie',
 
     // The data for our dataset
@@ -28,8 +29,8 @@ var CHART2 = new Chart(pieChart , {
     // Configuration options go here
     options: {}
 });
-const barChart =document.getElementById("bar-chart") ; 
-var CHART3 = new Chart(barChart , {
+const barChart = document.getElementById("bar-chart");
+var CHART3 = new Chart(barChart, {
     type: 'bar',
 
     // The data for our dataset
@@ -38,3 +39,19 @@ var CHART3 = new Chart(barChart , {
     // Configuration options go here
     options: {}
 });
+
+function slideInOut() {
+    console.log("hi there")
+    let sideMenu = document.querySelector("#side-bar");
+    let slideout = document.querySelector("#out");
+    let slidein = document.querySelector("#in");
+    slidein.addEventListener("click", function (e) {
+        e.preventDefault();
+        sideMenu.classList.remove("slide-out");
+        sideMenu.classList.add("slide-in");
+    });
+    slideout.addEventListener("click", function (e) {
+        sideMenu.classList.remove("slide-in");
+        sideMenu.classList.add("slide-out");
+    })
+}
