8273f30d2d0179aa09e89856abb4a6b105c8ea5f
hazem hemaily <hazem.hemily@gmail.com>
2018-06-28 14:52:16 -0700

    canvas issues solutions

--- AMS/src/app/core/filteration/filteration.component.ts
+++ AMS/src/app/core/filteration/filteration.component.ts
@@ -1,19 +1,20 @@
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { StudentsInLocationService } from '../services/student_in_location.service';
 import { StudentsInLocationModel } from '../models/student_in_location.model';
+import { Chart } from "chart.js";
 
 @Component({
   selector: 'app-filteration',
   templateUrl: './filteration.component.html',
   styleUrls: ['./filteration.component.css']
 })
-export class FilterationComponent implements OnInit {
+export class FilterationComponent implements OnInit  , AfterViewInit{
 
-  
-
+  @ViewChild('canvas') canvas : ElementRef; 
+  chart: Chart;
   filterForm: FormGroup;
-  studentInLocation: StudentsInLocationModel [];
+  studentInLocation: StudentsInLocationModel[];
 
 
 
@@ -21,72 +22,80 @@ export class FilterationComponent implements OnInit {
 
   ngOnInit() {
     this.filterForm = new FormGroup({
-      'fromdate' : new FormControl(null, Validators.required),
-      'todate' : new FormControl(null, Validators.required)
+      'fromdate': new FormControl(null, Validators.required),
+      'todate': new FormControl(null, Validators.required)
     });
   }
+ ngAfterViewInit(){
 
-  onSubmit(){
-    var absent:number = 0;
-    var present:number = 0;
+ }
+  onSubmit() {
+    var absent: number = 0;
+    var present: number = 0;
     var from = new Date(Date.parse(this.filterForm.get('fromdate').value));
     var to = new Date(Date.parse(this.filterForm.get('todate').value));
     this.studentInLocationService.getStudentsInLocation().subscribe(
-      (response) =>{
+      (response) => {
         this.studentInLocation = response.data;
-        
-
-        for(let student of this.studentInLocation){
+        for (let student of this.studentInLocation) {
           var created = new Date(Date.parse(student.created_at.toString()));
-          if(created.getTime() >= from.getTime()&&   
-           created.getTime() <= to.getTime()){
+          if (created.getTime() >= from.getTime() &&
+            created.getTime() <= to.getTime()) {
             console.log(student.id);
 
-            if(student.status === "0"){
+            if (student.status === "0") {
               absent++;
             }
-            else if (student.status === "1"){
+            else if (student.status === "1") {
               present++;
-            }else{
+            } else {
               console.log("Error happens in Logic");
             }
 
-
-
-
-           }
-
-           
-
-          // var sCreated = Math.round(student.created_at.getTime()/1000);
-         
           }
-      
 
-          console.log(present + " is Present");
-          console.log(absent + " is Absent");
+        }
+        console.log(this.canvas) ;
+        let ctx = this.canvas.nativeElement.getContext("2d");
+        this.chart = new Chart(ctx,
+          {
+            type: 'pie',
 
+            // The data for our dataset
+            data: {
+              labels: ["present", "absence"],
+              datasets: [{
+                label: "My First dataset",
+                fill: false,
+                // backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [present , absent ],
+              // },
+              // {
+              //   label: "My First dataset",
+              //   fill: false,
+              //   // backgroundColor: 'rgb(255, 99, 132)',
+              //   borderColor: 'rgb(255, 99, 002)',
+              //   data: [absent],
+              }
+              ]
+            },
 
-
-
-
-     //   console.log(this.studentInLocation);
-
-
-
-
-     } ,
-     (error) => console.log(error)
+            // Configuration options go here
+            options: {}
+          }
+        )
+        console.log(present + " is Present");
+        console.log(absent + " is Absent");
+      },
+      (error) => console.log(error)
     );
-
-   
-    
 
   }
 
 
   // filterStudentInLocation(sutudentsInLocation:StudentsInLocationModel []){
-   
+
   //  var arr : StudentsInLocationModel [];
   //   for(let student of this.studentInLocation){
   //       if(student.created_at < this.filterForm.get('fromdate').value &&
--- AMS/src/assets/js/main.js
+++ AMS/src/assets/js/main.js
@@ -1,70 +1,70 @@
-slideInOut();
-const pieChart = document.getElementById("myPieChart");
-var CHART2 = new Chart(pieChart, {
-    type: 'pie',
+// slideInOut();
+// const pieChart = document.getElementById("myPieChart");
+// var CHART2 = new Chart(pieChart, {
+//     type: 'pie',
 
-    // The data for our dataset
-    data:{
-        labels : ['Present','Absent'],
-        datasets: [
-            {
-                label: "My Pie Points",
-                backgroundColor: ['green','red'],
-                 borderColor: 'rgb(255, 99, 132)',
-                data: [10,55]
-            }
-        ]
-    }
+//     // The data for our dataset
+//     data:{
+//         labels : ['Present','Absent'],
+//         datasets: [
+//             {
+//                 label: "My Pie Points",
+//                 backgroundColor: ['green','red'],
+//                  borderColor: 'rgb(255, 99, 132)',
+//                 data: [10,55]
+//             }
+//         ]
+//     }
 
-    // Configuration options go here
-  // options: {}
-});
+//     // Configuration options go here
+//   // options: {}
+// });
 
 
-var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [{
-        label: "My First dataset",
-        fill: false,
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
-    }]
-};
-const lineChart = document.getElementById("line-chart");
-var CHART1 = new Chart(lineChart, {
-    type: 'line',
+// var data = {
+//     labels: ["January", "February", "March", "April", "May", "June", "July"],
+//     datasets: [{
+//         label: "My First dataset",
+//         fill: false,
+//         backgroundColor: 'rgb(255, 99, 132)',
+//         borderColor: 'rgb(255, 99, 132)',
+//         data: [0, 10, 5, 2, 20, 30, 45],
+//     }]
+// };
+// const lineChart = document.getElementById("line-chart");
+// var CHART1 = new Chart(lineChart, {
+//     type: 'line',
 
-    // The data for our dataset
-    data: data,
+//     // The data for our dataset
+//     data: data,
 
-    // Configuration options go here
-    options: {}
-});
+//     // Configuration options go here
+//     options: {}
+// });
 
-const barChart = document.getElementById("bar-chart");
-var CHART3 = new Chart(barChart, {
-    type: 'bar',
+// const barChart = document.getElementById("bar-chart");
+// var CHART3 = new Chart(barChart, {
+//     type: 'bar',
 
-    // The data for our dataset
-    data: data,
+//     // The data for our dataset
+//     data: data,
 
-    // Configuration options go here
-    options: {}
-});
+//     // Configuration options go here
+//     options: {}
+// });
 
-function slideInOut() {
-    console.log("hi there")
-    let sideMenu = document.querySelector("#side-bar");
-    let slideout = document.querySelector("#out");
-    let slidein = document.querySelector("#in");
-    slidein.addEventListener("click", function (e) {
-        e.preventDefault();
-        sideMenu.classList.remove("slide-out");
-        sideMenu.classList.add("slide-in");
-    });
-    slideout.addEventListener("click", function (e) {
-        sideMenu.classList.remove("slide-in");
-        sideMenu.classList.add("slide-out");
-    })
-}
+// function slideInOut() {
+//     console.log("hi there")
+//     let sideMenu = document.querySelector("#side-bar");
+//     let slideout = document.querySelector("#out");
+//     let slidein = document.querySelector("#in");
+//     slidein.addEventListener("click", function (e) {
+//         e.preventDefault();
+//         sideMenu.classList.remove("slide-out");
+//         sideMenu.classList.add("slide-in");
+//     });
+//     slideout.addEventListener("click", function (e) {
+//         sideMenu.classList.remove("slide-in");
+//         sideMenu.classList.add("slide-out");
+//     })
+// }
